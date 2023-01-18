e12b16fa9081c899902e4c73ed5ef155ed85bbf9
Giovanni Perotto <giovanniperotto@hotmail.com>
2023-01-18 17:08:28 -0300

    feat: create develop branch and create theme.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from './styles/theme'
+
 export function App() {
   return (
-    <h1>App</h1>
+    <ChakraProvider theme={theme}>
+      <h1>App</h1>
+    </ChakraProvider>
   )
 }
-
- 
